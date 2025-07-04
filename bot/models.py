ba5ea6c9fcc789295d300e2fd9feb0615128d9bb
YuriyNik156 <yuriy.nik156@gmail.com>
2025-07-04 16:20:58 +0300

    delete several fails and add models

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class TelegramUser(models.Model):
+    user_id = models.BigIntegerField(unique=True)
+    username = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.user_id})"
