2f8de89c32b962b61071d1b37d847c6e564d5a41
YuriyNik156 <yuriy.nik156@gmail.com>
2025-07-04 22:04:36 +0300

    final updates

--- bot_main.py
+++ bot_main.py
@@ -7,7 +7,6 @@ BOT_TOKEN = "7781641058:AAHZ1-gHQrZvSxYk-murPvIR1gwyomiCQLw"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-
 @bot.message_handler(commands=['start'])
 def start_command(message):
     data = {
@@ -15,18 +14,28 @@ def start_command(message):
         "username": message.from_user.username
     }
     response = requests.post(API_URL + "/register/", json=data)
-    if not response.status_code == 200:
+    if response.status_code == 200:
         if response.json().get('message'):
             bot.send_message(message.chat.id, "Вы уже были зарегистрированы ранее!")
         else:
-            bot.send_message(message.chat.id,
-                             f"Вы успешно зарегистрированы! Ваш уникальный номер: {response.json()['id']}")
+            bot.send_message(message.chat.id, f"Вы успешно зарегистрированы! Ваш уникальный номер: {response.json()['id']}")
     else:
-        bot.send_message(message.chat.id, f"Произошла ошибка ри регистрации!")
+        bot.send_message(message.chat.id, f"Произошла ошибка при регистрации!")
         print(response.json())
         print(response.status_code)
         print(response.text)
 
+@bot.message_handler(commands=['myinfo'])
+def user_info(message: Message):
+    response = requests.get(f"{API_URL}/user/{message.from_user.id}/")
+    if response.status_code == 200:
+        bot.reply_to(message, f"Ваша регистрация:\n\n{response.json()}")
+    elif response.status_code == 404:
+        bot.send_message(message.chat.id, text= "Вы не зарегистрированы!")
+    else:
+        bot.send_message(message.chat.id, text= "Непредвиденная ошибка!")
+        print("Код:", response.status_code)
+        print("Ответ:", response.text)
 
-if __name__ == "__main__":
+if __name__== "__main__":
     bot.polling(none_stop=True)
